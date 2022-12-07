2941e36bd9fad8b41b66bc4479df132996cd5adf
Dora Sun <lobster1216@gmail.com>
2022-12-06 23:33:18 -0500

    oops didn't add all the files haven't done git from cmd in a while

--- 2022/day7/part1.js
+++ 2022/day7/part1.js
@@ -1,13 +1,9 @@
 const fs = require("fs");
 var score = 0;
 
-function main(){
-	fs.readFile("input.txt", 'utf8', (err, data) => {
-		data.split("\n").forEach(line => {
-			
-		});
-		console.log(score);
-	})
-}
-
-main();
+fs.readFile("input.txt", 'utf8', (err, data) => {
+	data.split("\n").forEach(line => {
+		
+	});
+	console.log(score);
+})
