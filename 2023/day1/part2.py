43d07cd7f0522220dc4a10b16b02df50f2c09815
Dora Sun <lobster1216@gmail.com>
2023-12-01 01:18:32 -0500

    using a loop like a civilized person

--- 2023/day1/part2.py
+++ 2023/day1/part2.py
@@ -1,4 +1,3 @@
-import re
 import numpy as np
 file = open('input.txt', 'r')
 lines = file.readlines()
@@ -6,73 +5,30 @@ lines = file.readlines()
 total = 0
 
 for line in lines:
-	index1 = line.find("1")
-	index2 = line.find("2")
-	index3 = line.find("3")
-	index4 = line.find("4")
-	index5 = line.find("5")
-	index6 = line.find("6")
-	index7 = line.find("7")
-	index8 = line.find("8")
-	index9 = line.find("9")
+	searchTerms = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-	indexOne = line.find("one")
-	indexTwo = line.find("two")
-	indexThree = line.find("three")
-	indexFour = line.find("four")
-	indexFive = line.find("five")
-	indexSix = line.find("six")
-	indexSeven = line.find("seven")
-	indexEight = line.find("eight")
-	indexNine = line.find("nine")
-	indices = np.array([-1, index1, index2, index3, index4, index5, index6, index7, index8, index9, -1, indexOne, indexTwo, indexThree,indexFour, indexFive, indexSix, indexSeven,indexEight,indexNine])
+	firstMatch = len(line)
+	lastMatch = 0
+
+	for term in searchTerms:
+		firstIndex = line.find(term)
+		if firstIndex >= 0 and firstIndex < firstMatch:
+			firstMatch = firstIndex
+			tensValue = term
+
+		lastIndex = line.rfind(term)
+		if lastIndex >= lastMatch:
+			lastMatch = lastIndex
+			onesValue = term
 
 	print(line)
-	print(indices)
-	tensPlace = list(indices).index(min(indices[indices>=0]))
-	print(tensPlace)
-	if tensPlace > 10:
-		tensPlace -= 10
 
+	tensValue = tensValue.replace("one", "1").replace("two", "2").replace("three", "3").replace("four", "4").replace("five", "5").replace("six", "6").replace("seven", "7").replace("eight","8").replace("nine", "9")
+	onesValue = onesValue.replace("one", "1").replace("two", "2").replace("three", "3").replace("four", "4").replace("five", "5").replace("six", "6").replace("seven", "7").replace("eight","8").replace("nine", "9")
+	print(tensValue)
+	print(onesValue)
 
-
-	index1 = line.rfind("1")
-	index2 = line.rfind("2")
-	index3 = line.rfind("3")
-	index4 = line.rfind("4")
-	index5 = line.rfind("5")
-	index6 = line.rfind("6")
-	index7 = line.rfind("7")
-	index8 = line.rfind("8")
-	index9 = line.rfind("9")
-
-	indexOne = line.rfind("one")
-	indexTwo = line.rfind("two")
-	indexThree = line.rfind("three")
-	indexFour = line.rfind("four")
-	indexFive = line.rfind("five")
-	indexSix = line.rfind("six")
-	indexSeven = line.rfind("seven")
-	indexEight = line.rfind("eight")
-	indexNine = line.rfind("nine")
-	indices = np.array([-1, index1, index2, index3, index4, index5, index6, index7, index8, index9, -1, indexOne, indexTwo, indexThree,indexFour, indexFive, indexSix, indexSeven,indexEight,indexNine])
-
-
-
-
-
-	onesPlace = list(indices).index(max(indices))
-	print(onesPlace)
-	if onesPlace > 10: 
-		onesPlace -= 10
-	#onesPlace = indices.index(max(indices))
-
-	# print(line.strip())
-
-	# ints = [int(s) for s in re.findall(r'\d', line)]
-	# print(ints)
-
-	calibration = tensPlace*10 + onesPlace
+	calibration = int(tensValue)*10 + int(onesValue)
 	print(calibration)
 	total += calibration
 
